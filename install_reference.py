322d5f16a04dfff1115acdadddc45892a42d4966
Eric T. Dawson <eric.t.dawson@gmail.com>
2020-02-27 21:29:45 -0500

    change return on error

--- install_reference.py
+++ install_reference.py
@@ -10,5 +10,4 @@ def parse_args():
 if __name__ == "__main__":
 
     args = parse_args()
-    try:
-        genInstall.install(args.ref, rsync=False, bash=True)
+    genInstall.install(args.ref, rsync=False, bash=True)
