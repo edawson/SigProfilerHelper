f77b30d67850de4edf23b083d511b989315622e0
Eric Dawson <edawson@nvidia.com>
2024-02-08 00:16:46 -0600

    [run_sigprofiler] Update args to better reflect those of sigprofiler.

--- run_sigprofiler.py
+++ run_sigprofiler.py
@@ -8,12 +8,14 @@ from SigProfilerExtractor import sigpro as sig
 def parse_args():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input-table", dest="table", help="Input mutational catalog table (see README.md for format description).", required=True)
+    parser.add_argument("-t", "--table", dest="table", help="Input mutational catalog table (see README.md for format description).", required=True)
     parser.add_argument("-d", "--directory", dest="directory", help="A directory name in which to run analysis.", required=False, default=None, type=str)
     parser.add_argument("-r", "--reference",dest="genome", default="GRCh37", type=str)
     parser.add_argument("-s", "--start-sig-number",dest="start", type=int, default=1)
     parser.add_argument("-e", "--end-sig-number",dest="end", type=int, default=10)
-    parser.add_argument("-i", "--iterations",dest="iters", type=int, default=1000)
+    parser.add_argument("--nmf-replicates", dest="nmf_replicates", type=int, default=100)
+    parser.add_argument("--min-iters",dest="min_iters", type=int, default=1000)   
+    parser.add_argument("--max-iters",dest="max_iters", type=int, default=10000)
     parser.add_argument("-c", "--cpu", dest="cpu", type=int, default=-1, )
     parser.add_argument("-m", "--mtype",dest="mtype", type=str, default="96")
     parser.add_argument("-G", "--gpu", dest="gpu", action="store_true", default=False, help="Use an available GPU for acceleration.")
@@ -41,9 +43,10 @@ if __name__ == "__main__":
     sig.sigProfilerExtractor("table",
         args.directory,
         args.table,
-        startProcess=args.start,
-        endProcess=args.end,
-        totalIterations=args.iters,
+        minimum_signatures=args.start,
+        maximum_signatures=args.end,
+        min_nmf_iterations=args.min_iters,
+        max_nmf_iterations=args.max_iters,
         cpu=args.cpu,
-        mtype=args.mtype,
+        context_type=args.mtype,
         gpu=args.gpu)
